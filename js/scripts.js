d41bb2b3ed2e1a337874881d7f33c5a5948dcca8
Rafał Białek <bialekrafal94@gmail.com>
2018-04-07 00:50:27 +0200

    add js bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 var model = {
     boardSize: 7,
     numShips: 3,
-    shipLenght: 3,
+    shipLength: 3,
     shipSunk: 0,
 
     ships: [    
-                { locations: ["06", "16", "26"], hits: [" "," "," "] },
+                { locations: ["06", "16", "26"], hits: ["","",""] },
                 { locations: ["24", "34", "44"], hits: ["","",""] },
-                { locations: ["10", "11", "12"], hits: ["","",""] }                  
+                { locations: ["10", "11", "12"], hits: ["","",""] }
             ],
 
     fire: function(guess) {
@@ -23,39 +23,49 @@ var model = {
                 
                 if (this.isSunk(ship)) {
                     view.displayMessage("you sunk my ship");
-                    this.shipsSunk++;
+                    console.log("test");
+                    this.shipSunk++;
+
                     
                 }
                 return true;
 
             }
-
         }
-    view.displayMiss(guess);
-    view.displayMessage("miss");
-    return false;
+        view.displayMiss(guess);
+        view.displayMessage("miss");
+        return false;
 
     },
 
     isSunk: function(ship) {
-        for (var i = 0; i < this.shipLenght; i++) {
+        for (var i = 0; i < this.shipLength; i++)  {
             if (ship.hits[i] !== "hit") {
                 return false;
-            }
-            return true;
 
+            }
         }
+        return true;
+
 
     }
-
 };
 
 var controller = {
     guesses: 0,
-    processGuess: function(guess) {
-        
-    }
 
+    processGuess: function(guess) {
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipSunk === model.numShips) {
+                alert("you sunk all my ships, after " + this.guesses + " shots.");
+                window.location.reload();
+
+            }
+        }
+    }
 };
 
 function parseGuess(guess) {

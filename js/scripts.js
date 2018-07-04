3f84e3ae1e2288eeed1f96fb0b545b50dcbba23b
bialek <bialekrafal94@gmail.com>
2018-07-04 13:25:10 +0200

    random generate ship logic click to shot bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,57 @@
 var model = {
     boardSize: 7,
     numShips: 3,
-    shipLength: 3,
+    shipLength: 3, 
     shipSunk: 0,
-
     ships: [    
-                { locations: ["06", "16", "26"], hits: ["","",""] },
-                { locations: ["24", "34", "44"], hits: ["","",""] },
-                { locations: ["10", "11", "12"], hits: ["","",""] }
+                { locations: [0, 0, 0], hits: ["","",""] },
+                { locations: [0, 0, 0], hits: ["","",""] },
+                { locations: [0, 0, 0], hits: ["","",""] }
             ],
+    
+    generateShipLocations: function() {
+        var locations;
+        for (var i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip();
+            } while (this.collision(locations));
+            this.ships[i].locations = locations;
+        }
+    },
+
+    generateShip: function() {
+        var direction = Math.floor(Math.random() * 2);
+        var row, col;       
+        if (direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+        
+        var newShipLocations = [];
+        for (var i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocations.push(row + "" +(col + i));
+            } else {
+                newShipLocations.push((row + i) + "" + col);   
+            }
+        }
+        return newShipLocations;
+    },
+
+    collision: function(locations) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = model.ships[i];
+            for (var j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
 
     fire: function(guess) {
 
@@ -23,19 +66,14 @@ var model = {
                 
                 if (this.isSunk(ship)) {
                     view.displayMessage("you sunk my ship");
-                    console.log("test");
-                    this.shipSunk++;
-
-                    
+                    this.shipSunk++;                 
                 }
                 return true;
-
             }
         }
         view.displayMiss(guess);
         view.displayMessage("miss");
         return false;
-
     },
 
     isSunk: function(ship) {
@@ -46,49 +84,8 @@ var model = {
             }
         }
         return true;
-
-
     }
 };
-
-var controller = {
-    guesses: 0,
-
-    processGuess: function(guess) {
-        var location = parseGuess(guess);
-        if (location) {
-            this.guesses++;
-            var hit = model.fire(location);
-            if (hit && model.shipSunk === model.numShips) {
-                alert("you sunk all my ships, after " + this.guesses + " shots.");
-                window.location.reload();
-
-            }
-        }
-    }
-};
-
-function parseGuess(guess) {
-    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
-
-    if (guess === null || guess.length !== 2) {
-        alert("Ups, please put big letter and number.");
-    } else {
-        var firstChar = guess.charAt(0);
-        var row = alphabet.indexOf(firstChar);
-        var column = guess.charAt(1);
-
-        if (isNaN(row) || isNaN(column)) {
-            alert("these are not Coordinates!");
-        } else if (row < 0 || row >= model.boardSize ||
-                   column < 0 || column >= model.boardSize) {
-            alert("Out of range");
-        } else {
-            return row + column;
-        }
-    }
-    return null;
-}
 
 var view = {
     displayMessage: function(msg) {
@@ -108,33 +105,28 @@ var view = {
 
 };
 
-
-
-
-function handleFireButton() {
-    var guessInput = document.getElementById("guessInput");
-    var guess = guessInput.value;
-    controller.processGuess(guess);
-
-    guessInput.value = "";
-}
-
-function handleKeyPress(e) {
-    var fireButton = document.getElementById("fireButton");
-    e = e || window.event;
-    if (e.keyCode === 13) {
-        fireButton.click();
-        return false;
+var controller = {
+    guesses: 0,
+    shots: [],
+    processGuess: function(guess) {
+        var location = guess;
+        if (this.shots.includes(location)) {
+            alert("Już tu strzelałeś");
+        } else {
+            this.shots.push(location);
+        }
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipSunk === model.numShips) {
+                alert("Zatopiłeś moje statki po " + this.guesses + " Strzałach. Twoja skuteczność to " + Math.floor(100 / ((this.guesses / model.numShips) / model.numShips)) + "%");
+                view.displayMessage("Kliknij tutaj, by zrestartować");
+            }
+        }
     }
-} 
+};
 
 
-window.onload = init;
 
-function init() {
-    var fireButton = document.getElementById("fireButton");
-    fireButton.onclick = handleFireButton;
-    var guessInput = document.getElementById("guessInput");
-    guessInput.onkeypress = handleKeyPress;
+window.onload = model.generateShipLocations();
 
-}
